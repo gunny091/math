13a3c1839a7d62bd369a25f58a945ee2bddba812
gunny091 <ryuee091119@naver.com>
2022-03-12 10:34:53 +0900

    html,js 다만들었다! 이제 cssgit add .

--- sosu-soinsubunhae/app.js
+++ sosu-soinsubunhae/app.js
@@ -1,4 +1,5 @@
-const sosu_soinsubunhae = {
+const ssisbh = {
+    // 소수인지 확인
     isSosu: function (number) {
         if ((number <= 1) | !Number.isInteger(number)) {
             return false;
@@ -11,16 +12,17 @@ const sosu_soinsubunhae = {
         }
         return true;
     },
-
+    // 다음 소수
     nextSosu: function (number) {
         if ((number <= 1) | !Number.isInteger(number)) {
             return false;
         }
         result = number + 1;
         for (; !this.isSosu(result); result++) {}
-        console.log(result);
+        return result;
     },
-    soinsubunhae: function (number) {
+    // 소인수분해
+    sisbh: function (number) {
         let result = {};
 
         while (number % 2 == 0) {
@@ -48,5 +50,58 @@ const sosu_soinsubunhae = {
         }
         return result;
     },
-    soinsubunhaeToText: function (object) {},
+    // 소인수분해한 것 html로 변환
+    sisbhToHTML: function (object) {
+        const times = "&times;";
+
+        keys = Object.keys(object);
+        let result = "";
+        keys.forEach(element => {
+            if (object[element] <= 1) {
+                result += `${element} ${times} `;
+            } else {
+                result += `${element}<sup>${object[element]}</sup> ${times} `;
+            }
+        });
+        if (result) {
+            result = result.slice(0, -1 - times.length);
+        }
+        return result;
+    },
 };
+
+const inputInput = document.querySelector("input#input");
+const resultP = document.querySelector("p#result");
+const maxlength = 5;
+
+resultP.innerHTML = "입력하세요.";
+inputInput.addEventListener("input", () => {
+    inputValue = inputInput.value;
+    // 글자수 조정
+    if (inputValue.length > maxlength) {
+        inputInput.value = inputValue.substr(0, maxlength);
+        inputValue = inputInput.value;
+    }
+
+    // 아무것도 입력 안했을떄
+    if (inputValue === "") {
+        resultP.innerHTML = "입력하세요.";
+        return;
+    }
+    const inputValueInt = parseInt(inputValue);
+    // 정수가 아닐때
+    if (!(parseFloat(inputValue) % 1 === 0)) {
+        resultP.innerHTML = "정수를 입력하세요.";
+        return;
+    }
+    // 1보다 작거나 같을떄
+    if (inputValue.length <= 1) {
+        resultP.innerHTML = "2 이상의 수를 입력하세요.";
+        return;
+    }
+
+    resultP.innerHTML = `${ssisbh.sisbhToHTML(ssisbh.sisbh(inputValueInt))}`;
+    if (ssisbh.isSosu(inputValueInt)) {
+        resultP.innerHTML += "<br>소수입니다.";
+    }
+});
